5b7cb3d5ca73e2fdaeab68e45d15bd74147ae58b
SylvainDev <sylvain.raveneau1@gmail.com>
2025-01-30 16:16:55 +0100

    modification: ajout de hooks pour le hachage des mots de passe lors de la création et de la mise à jour des rôles

--- models/Role.js
+++ models/Role.js
@@ -2,7 +2,7 @@ import sequelize from '../config/sequelize.js';
 import { DataTypes } from 'sequelize';
 import User from './User.js';
 
-const Role = sequelize.define('Role', {
+export const Role = sequelize.define('Role', {
     idRole: {
       type: DataTypes.INTEGER,
       primaryKey: true,
@@ -23,13 +23,27 @@ const Role = sequelize.define('Role', {
       onDelete: 'CASCADE',
     },
   },
-  {
-    tableName: 'role',
-    timestamps: false,
-});
+    //options
+    {
+      hooks: {
+        beforeCreate: async (user) => {
+          if (user.password) {
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(user.password, salt);
+          }
+        },
+        beforeUpdate: async (user) => {
+          if (user.changed('password')) {
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(user.password, salt);
+          }
+        }
+      },
+      tableName: 'role', // Nom exact de la table dans la BDD
+      schema: 'Utilisateurs', // Nom du schéma de la table
+      timestamps: false, // Désactive les colonnes `createdAt` et `updatedAt` par défaut
+    });
 
 // Définition de la relation entre Utilisateur et Role
 Utilisateur.hasOne(Role, { foreignKey: 'idUtilisateur', as: 'role' });
 Role.belongsTo(User, { foreignKey: 'idUtilisateur', as: 'utilisateur' });
-
-export default Role;
