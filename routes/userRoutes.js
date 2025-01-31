0e0c31f309fd8a53d9fd62ac2f1f82c7ef8507bc
SylvainDev <sylvain.raveneau1@gmail.com>
2025-01-31 15:32:47 +0100

    refactor: suppression de l'importation de authenticateJWT dans userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,6 @@
 import { Router } from 'express';
 import { User } from '../models/User.js';
-import { authenticateJWT } from '../middleware/auth.js';
-// import bcrypt from 'bcrypt';
-// import { Role } from '../models/Role.js';
+// import { authenticateJWT } from '../middleware/auth.js';
 
 const router = Router();
 
