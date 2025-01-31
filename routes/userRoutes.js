2efbd68825547009283ca9ed24372d5d1abf338b
SylvainDev <sylvain.raveneau1@gmail.com>
2025-01-31 14:37:03 +0100

    modification de l'importation de la fonction authenticateToken en authenticateJWT dans userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { User } from '../models/User.js';
-import { authenticateToken } from '../middleware/auth.js';
+import { authenticateJWT } from '../middleware/auth.js';
 // import bcrypt from 'bcrypt';
 // import { Role } from '../models/Role.js';
 
